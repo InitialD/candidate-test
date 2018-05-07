11dd13802c196b7d94c19634e6c866808c24d99f
Adesh T <adeshtotaram@live.com>
2018-05-07 09:34:48 -0400

    Validating Registration
    Validating a simple registration and alerting if something is not valid

--- front-end/src/app/components/register/register.component.ts
+++ front-end/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(private ValidateService: ValidateService, private flashMessage:FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user ={
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+    //Fields
+    if(!this.ValidateService.validateRegister(user)){
+      this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+    //check email
+    if(!this.ValidateService.validateEmail(user.email)){
+      this.flashMessage.show('Please use valid email', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
   }
 
 }
