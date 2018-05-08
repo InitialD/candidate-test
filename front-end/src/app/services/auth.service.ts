79bda673e9a5733e53c3d0dfc9727dc920a4727b
Adesh T <adeshtotaram@live.com>
2018-05-08 08:16:46 -0400

    Registration Fix, with login implement
    -had to import rxjs in the test.ts with reflect.js
    -implemented the login characteristic

--- front-end/src/app/services/auth.service.ts
+++ front-end/src/app/services/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Http, Headers} from '@angular/http';
 //import "rxjs/add/operators";
 import { map } from 'rxjs/operators';
-import {Observable} from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +14,8 @@ export class AuthService {
   constructor(private http:Http) { }
 
   // fix observable
-  
-  /*registerUser(user){
+
+  registerUser(user){
     let headers = new Headers();
     //add content type to headers
     headers.append('Content-Type', 'application/json');
@@ -24,5 +23,24 @@ export class AuthService {
     //return by post the request
     return this.http.post('http://localhost:3000/users/register',
       user, {headers: headers}).map(res => res.json());
-  }*/
+  }
+  authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user,{headers: headers})
+      .map(res => res.json());
+  }
+
+  storeUserData(token, user){
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 }
